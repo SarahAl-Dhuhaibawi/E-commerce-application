cb194eada91f407693283cd799278b479a766794
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-03-04 18:16:59 +0100

    Added firebase functionality
    All products are now fetched from firebase and displayed

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/**TODO
+ * Get all products from firebase
+ * Display all products*/
+
+const url = 'https://js2-mp3-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+
+getProducts(url)
+    .then(displayProducts);
+
+async function getProducts(url) {
+    const response = await fetch(url);
+    const products = await response.json();
+    console.log(products);
+    return products;
+}
+
+function displayProducts(products) {
+    products.forEach(product => {
+        console.log(product);
+        const container = document.querySelector('main');
+
+        const productCard = document.createElement('section');
+        productCard.classList.add('product-card');
+        container.appendChild(productCard);
+
+        const productImg = document.createElement('img');
+        productImg.src = product.img;
+        productCard.appendChild(productImg);
+
+        const productTitle = document.createElement('h3');
+        productTitle.innerText = product.name;
+        productCard.appendChild(productTitle);
+
+        const div = document.createElement('div');
+        productCard.appendChild(div);
+
+        const priceTag = document.createElement('h4');
+        priceTag.innerText = product.price + ' kr';
+        div.appendChild(priceTag);
+
+        const btn = document.createElement('button');
+        btn.innerText = 'Add to cart';
+        div.appendChild(btn);
+    });
+}
