1728a09153f396f3f96a607a51ff2f5d63e297f1
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-03-06 09:29:54 +0100

    Updated js main with oop functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-/**TODO
- * Get all products from firebase
- * Display all products*/
+/**TODO*/
 
 const url = 'https://js2-mp3-default-rtdb.europe-west1.firebasedatabase.app/products.json';
 
 getProducts(url)
-    .then(displayProducts);
+    .then(createProductObjects);
 
 async function getProducts(url) {
     const response = await fetch(url);
@@ -14,39 +12,74 @@ async function getProducts(url) {
     return products;
 }
 
-function displayProducts(products) {
-    products.forEach(product => {
-        console.log(product);
-        const container = document.querySelector('main');
-
-        const productCard = document.createElement('section');
-        productCard.classList.add('product-card');
-        container.appendChild(productCard);
-
-        const productImg = document.createElement('img');
-        productImg.src = product.img;
-        productCard.appendChild(productImg);
-
-        const productTitle = document.createElement('h3');
-        productTitle.innerText = product.name;
-        productCard.appendChild(productTitle);
-
-        const div = document.createElement('div');
-        productCard.appendChild(div);
-
-        const priceTag = document.createElement('h4');
-        priceTag.innerText = product.price + ' kr';
-        div.appendChild(priceTag);
-
-        if(product.saldo != 0){
-            const btn = document.createElement('button');
-            btn.innerText = 'Add to cart';
-            div.appendChild(btn);
-        }
-        else{
-            const p = document.createElement('p');
-            p.innerText = 'Out of stock';
-            div.appendChild(p);
-        }
+function createProductObjects(products){
+    products.forEach(product =>{
+        const productObj = new Product(product.img, product.name, product.price, product.saldo);
+        console.log(productObj);
     });
+}
+
+
+class Product{
+    #img;
+    #name;
+    #price;
+    #saldo;
+
+    constructor(img, name, price, saldo){
+        this.#img = img;
+        this.#name = name;
+        this.#price = price;
+        this.#saldo = saldo;
+        this.#displayProduct();
+    }
+
+    #displayProduct() {
+            const container = document.querySelector('main');
+    
+            const productCard = document.createElement('section');
+            productCard.classList.add('product-card');
+            container.appendChild(productCard);
+    
+            const productImg = document.createElement('img');
+            productImg.src = this.#img;
+            productCard.appendChild(productImg);
+    
+            const productTitle = document.createElement('h3');
+            productTitle.innerText = this.#name;
+            productCard.appendChild(productTitle);
+    
+            const div = document.createElement('div');
+            productCard.appendChild(div);
+    
+            const priceTag = document.createElement('h4');
+            priceTag.innerText = this.#price + ' kr';
+            div.appendChild(priceTag);
+    
+            if(this.#saldo != 0){
+                const btn = document.createElement('button');
+                btn.innerText = 'Add to cart';
+                div.appendChild(btn);
+                btn.addEventListener('click', () =>{
+                    //Lägg till produkten i kundvagnen
+                });
+            }
+            else{
+                const p = document.createElement('p');
+                p.innerText = 'Out of stock';
+                div.appendChild(p);
+            }
+    }
+    
+    getImg(){
+        return this.#img;
+    }
+
+    getName(){
+        return this.#name;
+    }
+
+    getPrice(){
+        return this.#price;
+    }
 }
