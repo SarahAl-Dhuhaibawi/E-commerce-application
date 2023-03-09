50810109326a9f8535de96dd8a5d5580310cb148
SarahAl-Dhuhaibawi <106531509+SarahAl-Dhuhaibawi@users.noreply.github.com>
2023-03-09 14:25:11 +0100

    add message

--- js/main.js
+++ js/main.js
@@ -1,79 +1,95 @@
 import { Product } from "./Product.js";
 import Cookies from "../node_modules/js-cookie/dist/js.cookie.mjs";
-const url = 'https://js2-mp3-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+const url =
+  "https://js2-mp3-default-rtdb.europe-west1.firebasedatabase.app/products.json";
 
+setNumberOfItemsInCart();
 
- setNumberOfItemsInCart();
-
-getProducts(url)
-    .then(createProductObjects);
+getProducts(url).then(createProductObjects);
 
 //Fetch products from firebase
 async function getProducts(url) {
-    const response = await fetch(url);
-    const products = await response.json();
-    console.log(products)
-    return products;
+  const response = await fetch(url);
+  const products = await response.json();
+  console.log(products);
+  return products;
 }
 
 //Create Product objects with info from firebase
 //Creates a cookie with the information from the object
 function createProductObjects(products) {
-    products.forEach(product => {
-        console.log(product)
-        const productObj = new Product(product.img, product.name, product.price, product.saldo);
+  products.forEach((product) => {
+    console.log(product);
+    const productObj = new Product(
+      product.img,
+      product.name,
+      product.price,
+      product.saldo
+    );
 
-        const btn = productObj.getBtn();
-        let amount = productObj.getAmount();
+    const btn = productObj.getBtn();
+    let amount = productObj.getAmount();
 
-        const productCard = productObj.getProductCard();
+    const productCard = productObj.getProductCard();
 
-        const cookieValue = Cookies.get(productObj.getName());
-        if (cookieValue) {
-            const storedProduct = JSON.parse(cookieValue);
-            amount = storedProduct.amount;
-            productObj.setAmount(amount);
+    const cookieValue = Cookies.get(productObj.getName());
+    if (cookieValue) {
+      const storedProduct = JSON.parse(cookieValue);
+      amount = storedProduct.amount;
+      productObj.setAmount(amount);
+    }
+
+    // Update amount if saldo is greater than stored amount
+    if (product.saldo != 0) {
+      btn.addEventListener("click", () => {
+        if (amount == product.saldo) {
+          console.log("Amount:", amount, "Saldo:", product.saldo);
+          console.log("There are no more products");
+          //Show message to user
+          const soldOutP = document.createElement("p");
+          productCard.appendChild(soldOutP);
+          soldOutP.innerText = "There are no more products";
+          soldOutP.style.backgroundColor = "#c80043";
+          soldOutP.style.color = "#fff";
+          //do not repeat message...
+          
+        } else {
+          amount++;
+          console.log("amount:", amount);
+          productObj.setAmount(amount);
+          console.warn("PRODUCT-OBJECT", productObj);
+
+          //Expires in 15 minutes
+          const expirationTime = new Date(
+            new Date().getTime() + 15 * 60 * 1000
+          );
+          Cookies.set(
+            productObj.getName(),
+            JSON.stringify(productObj.getInfo()),
+            { expires: expirationTime }
+          );
+          setNumberOfItemsInCart();
         }
-
-        // Update amount if saldo is greater than stored amount
-        if (product.saldo != 0) {
-            btn.addEventListener("click", () => {
-                if (amount == product.saldo) {
-                    console.log('Amount:', amount, 'Saldo:', product.saldo);
-                    console.log("There are no more products");
-                    //Show message to user
-                } else {
-                    amount++;
-                    console.log("amount:", amount);
-                    productObj.setAmount(amount);
-                    console.warn("PRODUCT-OBJECT", productObj);
-
-                    //Expires in 15 minutes
-                    const expirationTime = new Date(new Date().getTime() + 15 * 60 * 1000);
-                    Cookies.set(productObj.getName(), JSON.stringify(productObj.getInfo()), { expires: expirationTime });
-                    setNumberOfItemsInCart();
-                }
-            });
-        }
-    });
+      });
+    }
+  });
 }
 
-
 function getAllCookies() {
-    const allCookies = Cookies.get();
-    return allCookies;
+  const allCookies = Cookies.get();
+  return allCookies;
 }
 
 function setNumberOfItemsInCart() {
-    const allCookies = getAllCookies();
-    let nbrOfItems = 0;
-    for (const cookie in allCookies) {
-         const cookieObj = JSON.parse(allCookies[cookie]);
-        console.log(cookie)
-        console.log('Cookie object', cookieObj);
-        console.log(cookieObj.amount);
-        nbrOfItems += cookieObj.amount;
-    }
-    const span = document.querySelector('.shopping-cart-container span');
-    span.innerText = nbrOfItems;
+  const allCookies = getAllCookies();
+  let nbrOfItems = 0;
+  for (const cookie in allCookies) {
+    const cookieObj = JSON.parse(allCookies[cookie]);
+    console.log(cookie);
+    console.log("Cookie object", cookieObj);
+    console.log(cookieObj.amount);
+    nbrOfItems += cookieObj.amount;
+  }
+  const span = document.querySelector(".shopping-cart-container span");
+  span.innerText = nbrOfItems;
 }
