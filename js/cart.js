002262d7a7182b4c12668aef4af5e8c651cec803
SarahAl-Dhuhaibawi <106531509+SarahAl-Dhuhaibawi@users.noreply.github.com>
2023-03-09 10:58:21 +0100

    fix lite cart js

--- js/cart.js
+++ js/cart.js
@@ -1,86 +1,80 @@
 import anime from "../node_modules/animejs/lib/anime.es.js";
 import Cookies from "../node_modules/js-cookie/dist/js.cookie.mjs";
 
-const productsContainerParents = document.querySelector('.products-container');
-const cancelBtn = document.querySelector('.cancel-btn');
-const ThanksMessage = document.querySelector('.success-message-div')
-
-
+const productsContainerParents = document.querySelector(".products-container");
+const cancelBtn = document.querySelector(".cancel-btn");
+const ThanksMessage = document.querySelector(".success-message-div");
 
 shoppingListFromCookies();
 
 function shoppingListFromCookies() {
-    const allCookies = Cookies.get();
-    let totalPrice = 0;
-    for (const cookie in allCookies) {
-       const product = JSON.parse(allCookies[cookie]);
-       console.log(product);
-       createElements(product);
+  const allCookies = Cookies.get();
+  let totalPrice = 0;
+  for (const cookie in allCookies) {
+    const product = JSON.parse(allCookies[cookie]);
+    console.log(product);
+    createElements(product);
 
-       totalPrice += product.price * product.amount;
-    }
-    console.log(totalPrice);
-    const basketTotal = document.querySelector('.basketTotal');
-    basketTotal.innerText = totalPrice;
+    totalPrice += product.price * product.amount ;
+  }
+  console.log(totalPrice);
+  const basketTotal = document.querySelector(".basketTotal");
+  basketTotal.innerText = totalPrice +' ' + "kr";
 }
 
-function createElements(product){
-const productsCartContainer = document.querySelector('.products');
-const productsCartDiv = document.createElement('div');
-productsCartContainer.append(productsCartDiv);
-productsCartDiv.classList.add("productsCartDiv");
+function createElements(product) {
+  const productsCartContainer = document.querySelector(".products");
+  const productsCartDiv = document.createElement("div");
+  productsCartContainer.append(productsCartDiv);
+  productsCartDiv.classList.add("productsCartDiv");
 
-const imgAndNameDiv = document.createElement('div');
-productsCartDiv.append(imgAndNameDiv);
-imgAndNameDiv.classList.add('imgAndNameDiv')
-const productImg = document.createElement('img');
-imgAndNameDiv.append(productImg);
-productImg.src = product.img;
-const productName = document.createElement('h5');
-imgAndNameDiv.append(productName);
-productName.innerText = product.name;
-const productPrice = document.createElement('p');
-productsCartDiv.append(productPrice);
-productPrice.innerText = product.price +'kr';
-const productTotal = document.createElement('p');
-productsCartDiv.append(productTotal);
-productTotal.innerText = product.amount;
+  const imgAndNameDiv = document.createElement("div");
+  productsCartDiv.append(imgAndNameDiv);
+  imgAndNameDiv.classList.add("imgAndNameDiv");
+  const productImg = document.createElement("img");
+  imgAndNameDiv.append(productImg);
+  productImg.src = product.img;
+  const productName = document.createElement("h5");
+  imgAndNameDiv.append(productName);
+  productName.innerText = product.name;
+  const productPrice = document.createElement("p");
+  productsCartDiv.append(productPrice);
+  productPrice.innerText = product.price +' '+ "kr";
+  const productTotal = document.createElement("p");
+  productsCartDiv.append(productTotal);
+  productTotal.innerText = product.amount;
 
-const productTotalSum = document.createElement('p');
-productsCartDiv.append(productTotalSum);
-productTotalSum.innerText = `${product.price * product.amount}` + 'kr';
+  const productTotalSum = document.createElement("p");
+  productsCartDiv.append(productTotalSum);
+  productTotalSum.innerText = `${product.price * product.amount}` +' '+ "kr";
 
-
-showAndHideElements();
-loadingAnime();
-clearShoppingCart();
-
+  showAndHideElements();
+  loadingAnime();
+  clearShoppingCart();
 }
-//show and hide elements 
-function showAndHideElements(){
-    cancelBtn.style.display = 'block';
-    
-    productsContainerParents.style.display = 'block';
-    
-    document.querySelector('.cart-intro-div').style.display = 'none';
+//show and hide elements
+function showAndHideElements() {
+  cancelBtn.style.display = "block";
+
+  productsContainerParents.style.display = "block";
+
+  document.querySelector(".cart-intro-div").style.display = "none";
 }
 
 //loading animation
-function loadingAnime(){
-    document.querySelector('.checkout-btn').addEventListener('click', event =>{
-        event.preventDefault();
+function loadingAnime() {
+  document.querySelector(".checkout-btn").addEventListener("click", (event) => {
+    event.preventDefault();
 
-        const animation = document.querySelector("#animation");
-  const box = document.querySelector("#box");
-  box.style.display = 'block';
-  productsContainerParents.style = 'none';
-  cancelBtn.style.display = 'none';
-//   console.log(animation,box)
+    const animation = document.querySelector("#animation");
+    const box = document.querySelector("#box");
+    box.style.display = "block";
+    productsContainerParents.style = "none";
+    cancelBtn.style.display = "none";
+    //   console.log(animation,box)
 
-setTimeout(() => {
-    
-    
-    anime( {
+    setTimeout(() => {
+      anime({
         targets: "#box #animation",
         duration: 2000,
         easing: "linear",
@@ -91,36 +85,23 @@ setTimeout(() => {
         loop: true,
         direction: "alternate",
         delay: anime.stagger(300),
-      });"2000";
+      });
+      ("2000");
 
-      document.querySelector('.success-message-div').style.display = 'block';  // ani och message inte somtidigt
-    })
-}
-    )
+      document.querySelector(".success-message-div").style.display = "block"; // ani och message inte somtidigt
+    });
+  });
 }
 
 //clearShoppingCart
-function clearShoppingCart(){
-    cancelBtn.addEventListener('click', event=>{
-        box.style.display = 'none';
-        productsContainerParents.style = 'none';
+function clearShoppingCart() {
+  cancelBtn.addEventListener("click", (event) => {
+    box.style.display = "none";
+    productsContainerParents.style = "none";
 
-
-        //vi måste updatera vår storage
-    })
+    //vi måste updatera vår storage
+  });
 }
-
 
 //En knapp för att genomföra köpet (inte på riktigt)  ????? ändra storage
 //En knapp för att tömma kundvagnen ?????    ändra storage
-
-
-
-
-
-
-
-
-
-
-
